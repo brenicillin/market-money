a87fd67bdfbd864f9e6e83264a338eb93a7b8371
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-05-16 19:39:22 -0400

    add validation to models

--- app/models/market.rb
+++ app/models/market.rb
@@ -1,4 +1,7 @@
 class Market < ApplicationRecord
   has_many :market_vendors
   has_many :vendors, through: :market_vendors
+
+  validates_presence_of :name, :street, :city, :county, :state, :zip, :lat, :lon
+  validates_numericality_of :zip
 end
