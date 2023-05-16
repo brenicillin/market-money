136b414c1281a00e1215f259e9b6d35bbf53537e
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-05-16 15:25:18 -0400

    add show and index to marketscontroller

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -2,4 +2,12 @@ class Api::V0::MarketsController < ApplicationController
   def index
     render json: MarketSerializer.new(Market.all)
   end
+
+  def show
+    if Market.exists?(params[:id])
+      render json: MarketSerializer.new(Market.find(params[:id]))
+    else
+      render json: { errors: [{ detail: "Couldn't find market with 'id'=#{params[:id]}."}] }, status: 404
+    end
+  end
 end
