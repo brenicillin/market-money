43238e1ef14f1b8fe850bd68a1184c533cfba6ba
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-05-17 12:59:43 -0400

    add create and destroy actions to controller

--- app/controllers/api/v0/vendors_controller.rb
+++ app/controllers/api/v0/vendors_controller.rb
@@ -15,4 +15,27 @@ class Api::V0::VendorsController < ApplicationController
       render json: { errors: [{ detail: "Couldn't find Vendor with 'id'=#{params[:id]}."}] }, status: 404
     end
   end
+
+  def create
+    vendor = Vendor.new(vendor_params)
+    if vendor.save
+      render json: VendorSerializer.new(vendor), status: 201
+    else
+      render json: { errors: vendor.errors }, status: 400
+    end
+  end
+
+  def destroy
+    if Vendor.exists?(params[:id])
+      Vendor.destroy(params[:id])
+    else
+      render json: { errors: [{ detail: "Couldn't find Vendor with 'id'=#{params[:id]}."}] }, status: 404
+    end
+  end
+
+  private
+
+  def vendor_params
+    params.require(:vendor).permit(:name, :description, :contact_name, :contact_phone, :credit_accepted)
+  end
 end
