712165f055b51805ef99259cbc236fad6944b9c2
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-05-18 15:56:16 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
         resources :vendors, only: [:index]
       end
       resources :vendors, only: [:show, :create, :destroy, :update]
+      resources :market_vendors, only: [:create]
+      delete '/market_vendors', to: 'market_vendors#destroy'
     end
   end
 end
